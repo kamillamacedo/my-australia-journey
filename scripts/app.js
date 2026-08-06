0a38d5ded479c581c2f6fbf6cccbcf3adab264a4
Kamilla Macêdo <macedo.kamilla@outlook.com>
2026-08-05 23:00:11 -0300

    feat: materialize timeline-dot in HTML, setup event listener, and add comments data

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 import { openModal } from "./components/modal.js";
+import { openPopUp } from "./components/timeline.js";
 
-const destinationCards = document.querySelector(".destinations-cards");
+const destinationCards = document.querySelector(".destinations-cards");const timelineWrapper = document.querySelector(".timeline-wrapper");
+
 
 destinationCards.addEventListener("click", (event) => {
   const card = event.target.closest(".card__button");
   if (!card) return;
   openModal(card.dataset.destination);
 });
+
+timelineWrapper.addEventListener("mouseover", (e)=>{
+    const dot = e.target.closest(".timeline-dot");
+    if (!dot) return;
+     const event = dot.dataset.timelineId;
+    openPopUp(event);
+})
