8fb8642b6f23dcd57d6b190ace68db1a4917b67d
Kamilla Macêdo <macedo.kamilla@outlook.com>
2026-08-10 13:26:42 -0300

    rEVERt "feat: add active state highlight on click"
    This reverts commit 9112c531a8cc8e2ac3c60758e345bb75323b4d4f.

--- scripts/components/tips.js
+++ scripts/components/tips.js
@@ -1,23 +1,17 @@
-import { tipsObjectList } from "../data/tipsData.js";
+import { tipsObjectList } from "../data/tipsData";
 
 const tipsPanel = document.querySelector(".tips-content-panel");
 const tipsTitle = document.querySelector(".tips-panel-title");
 const tipsText = document.querySelector(".tips-panel-text");
 
 let activeTipsButton = null;
-let activeTipsElement = null;
 
 export function toggleTipsPanel(tipsCard, tipsId) {
   if (activeTipsButton === null || activeTipsButton !== tipsId) {
-    if (activeTipsButton !== null) {
-      activeTipsElement.classList.remove("active");
-    }
     const tipObj = tipsObjectList.find((tip) => tip.id === tipsId);
     tipsTitle.textContent = tipObj.title;
     tipsText.textContent = tipObj.tip;
     tipsPanel.removeAttribute("hidden");
-    activeTipsElement = tipsCard;
-    activeTipsElement.classList.add("active");
     activeTipsButton = tipsId;
   } else {
     closeTipsPanel();
@@ -26,10 +20,7 @@ export function toggleTipsPanel(tipsCard, tipsId) {
 }
 
 export function closeTipsPanel() {
-  if (!activeTipsElement) return;
   tipsPanel.setAttribute("hidden", "true");
-  activeTipsElement.classList.remove("active");
-  activeTipsElement = null;
   activeTipsButton = null;
   clearText();
 }
@@ -38,3 +29,4 @@ function clearText() {
   tipsTitle.textContent = "";
   tipsText.textContent = "";
 }
+
