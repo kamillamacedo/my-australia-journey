3170db3e584e2b1c7089a11b7dc22c435fc51669
Kamilla Macêdo <macedo.kamilla@outlook.com>
2026-09-03 21:57:10 -0300

    chore: migrate project structure to Vite

---
+++ src/scripts/components/footer.js
@@ -0,0 +1,44 @@
+const footerTemplate = `<div class="page-footer">
+        <div>
+          <a href="index.html" title="Go back to the main page">
+            <img
+              class="footer-logo"
+              src="/img/bottom-logo.png"
+              alt="Australia Journey logo colored in blue and red"
+            />
+          </a>
+          <p>Developed by Kamilla Macêdo</p>
+        </div>
+        <div class="footer-socialmedia">
+          <p>Access my networks:</p>
+          <div class="footer-icons">
+            <a href="https://www.linkedin.com/in/kamillamacedo" target="_blank" rel="noopener noreferrer" title="Access Kamilla Macêdo's Linkedin Profile"
+              ><img
+                src="/img/linkedin-icon.png"
+                alt="Social media icon for Linkedin"
+            /></a>
+            <a href="https://github.com/kamillamacedo" target="_blank" rel="noopener noreferrer" title="Access Kamilla Macêdo's GitHub Profile"
+              ><img
+                src="/img/github-icon.png"
+                alt="Social media icon for GitHub"
+            /></a>
+            <!--
+            <a
+              href="https://www.instagram.com/kamillamacedo"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Access Kamilla Macêdo's Instagram Profile"
+              ><img
+                src="/img/Instragam-icon.png"
+                alt="Social media icon for Instagram"
+            /></a>
+            -->
+          </div>
+        </div>
+      </div>`
+
+
+export function initFooter() {
+  const footerElement = document.querySelector("#main-footer");
+  footerElement.innerHTML = footerTemplate;
+}
---
+++ src/scripts/data/destinationsData.js
@@ -0,0 +1,62 @@
+export const destinations = [
+    {
+        id: "cottesloe",
+        title: "Cottesloe Beach",
+        date: "March 2010",
+        location: "Perth, Western Australia",
+        description: "Cottesloe Beach is one of the most famous beaches in Perth, Western Australia. It is known for its clear waters, white sand, and stunning sunsets. The beach is a popular spot for swimming, surfing, and picnicking. It also has a vibrant atmosphere with cafes and restaurants nearby.",
+        mainImage: "/img/destinations/maincottesloe.jpg",
+        gallery: [
+            "/img/destinations/cottesloeGallery1.jpg",
+            "/img/destinations/cottesloeGallery2.jpg",
+            "/img/destinations/cottesloeGallery3.jpg",
+            "/img/destinations/cottesloeGallery4.jpg",
+        ],
+        tags: ["Indiana Tea House", "Sunset view", "Swimming", "Beach"]
+    },
+    {
+        id: "hillarys",
+        title: "Hillarys Boat Harbour",
+        date: "October 2009",
+        location: "Perth, Western Australia",
+        description: "Hillarys Boat Harbour is a popular marina and tourist destination in Perth, Western Australia. It offers a range of activities including boating, fishing, and shopping. The harbour is home to the Aquarium of Western Australia (AQWA) and has a variety of restaurants and cafes. It's a great place for families and tourists to enjoy the coastal lifestyle.",
+        mainImage: "/img/destinations/mainhillarys.jpg",
+        gallery: [
+            "/img/destinations/hillarysGallery1.jpg",
+            "/img/destinations/hillarysGallery2.jpg",
+            "/img/destinations/hillarysGallery3.jpg",
+            "/img/destinations/hillarysGallery4.jpg",
+        ],
+        tags: ["AQWA", "Boating", "Fishing", "Shopping"]
+    },
+    {
+        id: "lancelin",
+        title: "Lancelin Sand Dunes",
+        date: "June 2010",
+        location: "Lancelin, Western Australia",
+        description: "Lancelin Sand Dunes are a natural wonder located in Lancelin, Western Australia. The dunes are popular for sandboarding and off-road driving. Visitors can climb the dunes to enjoy panoramic views of the surrounding area. The dunes are also a great spot for photography and experiencing the unique landscape of Western Australia.",
+        mainImage: "/img/destinations/mainlancelin.jpg",
+        gallery: [
+            "/img/destinations/lancelinGallery1.jpg",
+            "/img/destinations/lancelinGallery2.jpg",
+            "/img/destinations/lancelinGallery3.jpg",
+            "/img/destinations/lancelinGallery4.jpg",
+        ],
+        tags: ["Sandboarding", "Off-road driving", "Photography", "Unique landscape"]
+    },
+    {
+        id: "denmark",
+        title: "Denmark",
+        date: "November 2010",
+        location: "Denmark, Western Australia",
+        description: "Denmark is a charming town located in the Great Southern region of Western Australia. It is known for its beautiful beaches, lush forests, and vibrant arts scene. Visitors can explore the local wineries, enjoy outdoor activities such as hiking and fishing, and experience the town's unique culture and history. Denmark is a perfect destination for those looking to relax and connect with nature. There you can also visit the famous Greens Pool and Elephant Rocks, which are popular tourist attractions in the area. And you get to walk on the Valley of the Giants Tree Top Walk, a spectacular canopy walkway suspended 40 meters above the ground, right among the giants of the forest: the ancient and massive Red Tingle trees.",
+        mainImage: "/img/destinations/maindenmark.jpg",
+        gallery: [
+            "/img/destinations/denmarkGallery1.jpg",
+            "/img/destinations/denmarkGallery2.jpg",
+            "/img/destinations/denmarkGallery3.jpg",
+            "/img/destinations/denmarkGallery4.jpg",
+        ],
+        tags: ["Greens Pool", "Elephant Rocks", "Tree Top Walk", "Nature"]
+    }
+]
